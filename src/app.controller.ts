8583c2cfa907df5c2164d655d424d2ed9d5bd7c6
Dung Huynh <dunghuynh.010598@gmail.com>
2024-07-12 16:50:30 +0700

    Add API CRUD users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { AppService, User } from './app.service';
 
-@Controller()
+class CreateUserDto {
+  name: string
+}
+
+@Controller('users')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  find(): User[] {
+    return this.appService.find();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', new ParseIntPipe()) id: number): User {
+    return this.appService.findById(id);
+  }
+
+  @Post()
+  create(@Body() body: CreateUserDto) {
+    const { name } = body
+    const newUser = this.appService.create(name)
+
+    return newUser
+  }
+
+  @Patch(':id')
+  update(@Param('id', new ParseIntPipe()) id: number, @Body() body: CreateUserDto) {
+    const { name } = body
+    const updatedUser = this.appService.update(id, name)
+
+    return updatedUser
+  }
+
+  @Delete(':id')
+  delete(@Param('id', new ParseIntPipe()) id: number): boolean {
+    return this.appService.delete(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+
+export interface User {
+  id: number
+  name: string
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private users: User[] = []
+
+  find(): User[] {
+    return [...this.users.reverse()];
+  }
+
+  findById(id: number): User {
+    return this.users.find(el => el.id === id)
+  }
+
+  create(name: string): User {
+    const newUser = {
+      id: (this.users[this.users.length - 1]?.id ?? -1) + 1,
+      name
+    }
+    this.users.push(newUser)
+
+    return newUser
+  }
+
+  update(id: number, name: string): User {
+    const index = this.users.findIndex(el => el.id === id)
+    if (index === -1) {
+      throw new BadRequestException('user not found')
+    }
+
+    const newUser = {
+      ...this.users[index],
+      name
+    }
+    this.users[index] = newUser
+
+    return newUser
+  }
+
+  delete(id: number): boolean {
+    const index = this.users.findIndex(el => el.id === id)
+    if (index === -1) {
+      throw new BadRequestException('user not found')
+    }
+
+    this.users.splice(index, 1)
+    return true
   }
 }
